a122eebf3159627dfec2d5ed63e69ae2d9166e1f
TakumiFujita <46127387+TakumiFujita@users.noreply.github.com>
2023-12-13 21:55:40 +0900

    add purchase details table

---
+++ database/migrations/2023_12_13_213905_create_purchase_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_details', function (Blueprint $table) {
+            // 購入明細ID
+            $table->bigIncrements('purchased_detail_id');
+            // 商品ID
+            $table->unsignedBigInteger('item_id');
+            // 購入数
+            $table->unsignedInteger('purchase_number');
+            // 購入ID
+            $table->unsignedBigInteger('purchased_id');
+            //タイムスタンプ
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_details');
+    }
+};
